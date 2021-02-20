c6ba95e10b12fac85efeaef1f11474bde2304298
ElitaRe <er49779@universum-ks.org>
2021-02-20 21:45:09 +0100

    hw

--- Sort.js
+++ Sort.js
@@ -2,21 +2,25 @@
 // Sorting an array with sort.
 function start()
 {
-var a = [ 10, 1, 9, 2, 8, 3, 7, 4, 6, 5 ];
-outputArray( "Data items in original order: ", a,
-document.getElementById( "original Array" ) );
-a.sort( compareIntegers ); // sort the array
-outputArray( "Data items in ascending order:", a,
-document.getElementById( "sortedArray" ) );
-} // end function start
-// output the heading followed by the contents of theArray
+	var a = [10, 1, 9, 2, 8, 3, 7, 4, 6, 5];
+
+	outputArray( "Data items in original order:", a,
+		document.getElementById( "originalArray" ));
+	a.sort( compareIntegers ); // sort the array
+	outputArray( "Data items in ascending order:", a,
+		document.getElementById( "sortedArray" ));
+}	// end function start
+
+// output the headng followed by the contents of theArray
 function outputArray( heading, theArray, output )
 {
-output.innerHTML = heading + theArray.join();
-} // end function outputArray
+	output.innerHTML = heading + theArray.join( " ");
+}	// end function outputArray
+
 // comparison function for use with sort
-function compareIntegers( valuel, value2 )
+function compareIntegers( value1, value2 )
 {
-return parseInt( valuel ) - parseInt( value2 );
-} // end function compareIntegers
+	return parseInt( value1 )- parseInt( value2 );
+}	// end function compareIntegers
+
 window.addEventListener( "load", start, false );
